d5354c2a6ec968648d47ae80f1860f78fd861c3a
Ahmed Wael <127795744+mellegyy@users.noreply.github.com>
2025-04-18 02:54:43 +0200

    Captain waiting for trip

--- src/main/java/com/example/mini2/repositories/CaptainRepository.java
+++ src/main/java/com/example/mini2/repositories/CaptainRepository.java
@@ -1,4 +1,18 @@
 package com.example.mini2.repositories;
 
-public interface CaptainRepository {
+import com.example.mini2.models.Captain;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CaptainRepository extends JpaRepository<Captain, Long> {
+
+    @Query(value = "SELECT * FROM captain WHERE avg_rating_score > :threshold", nativeQuery = true)
+    public List<Captain> findCaptainsWithRating(@Param("threshold") double threshold);
+
+    public Captain findByLicenseNumber(String licenseNumber);
 }
