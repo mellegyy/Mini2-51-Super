059bcafae81022d25d7537a170f0d6a36b424f53
DaliaHesham <dalia.kamal@student.guc.edu.eg>
2025-04-19 04:05:23 +0200

    add rating model and rating repository

--- src/main/java/com/example/mini2/repositories/RatingRepository.java
+++ src/main/java/com/example/mini2/repositories/RatingRepository.java
@@ -1,4 +1,17 @@
 package com.example.mini2.repositories;
 
-public interface RatingRepository {
+import com.example.mini2.models.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RatingRepository extends MongoRepository<Rating, String> {
+
+    // Find by entityId and entityType
+    List<Rating> findByEntityIdAndEntityType(Long entityId, String entityType);
+
+    // Find ratings with a score above a certain value
+    List<Rating> findByScoreGreaterThan(int score);
 }
